9aed77b0e57b23109bda049d15db66e9797c86ea
Sand-a <s_anid@hotmail.com>
2023-06-15 11:09:39 +0100

    location

--- src/datas/contactData.js
+++ src/datas/contactData.js
@@ -8,7 +8,7 @@ export const contactData = {
   address:
     "Gallerie Au Clair de la Lune\n Nassif Building\n Mkalles Main Road\n Beitut - Lebanon",
   location_url:
-    "https://www.google.com/maps/place/33%C2%B051'51.2%22N+35%C2%B032'48.6%22E/@33.864231,35.546841,17z/data=!4m4!3m3!8m2!3d33.8642222!4d35.5468333?entry=ttu",
+    "https://www.google.com/maps/place/33%C2%B051'48.8%22N+35%C2%B032'49.2%22E/@33.8635691,35.5463696,19z/data=!4m4!3m3!8m2!3d33.863568!4d35.547012?entry=ttu",
   insta_1: "chirine_sayegh_architect",
   insta_1_url:
     "https://instagram.com/chirine_sayegh_architect?igshid=MzRlODBiNWFlZA==",
